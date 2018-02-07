c3cbbc496d034bfac3770520a51c93a1bd8c6539
jmick44 <jaime.mickelson@puppet.com>
2018-02-07 10:07:37 -0700

    Updated hostname

--- scripts/linux_agent.sh
+++ scripts/linux_agent.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -ex
-hostname ubuntu.vm
+hostname linux.vm
 echo '192.168.50.4 master.vm master' >> /etc/hosts
 echo '192.168.50.6 linux.vm linux' >> /etc/hosts
-echo '192.168.50.7 ubuntu.vm ubuntu' >> /etc/hosts
 curl -k https://master.vm:8140/packages/current/install.bash | bash
